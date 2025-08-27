4fc609027cbe6992a49d483882f3971bf13711ac
abazyshyn <maybebazishin@gmail.com>
2025-08-27 15:12:29 +0300

    chore: cmake for linux

--- lab/src/entry_point.cpp
+++ lab/src/entry_point.cpp
@@ -13,13 +13,6 @@ using namespace Lab;
 
 int main(int argc, char **argv)
 {
-    // TODO: move it somewhere
-    // #if defined(LAB_DEBUG) || defined(LAB_DEVELOPMENT)
-    //
-    //     testing::InitGoogleTest(&argc, argv);
-    //
-    // #endif // defined(LAB_DEBUG) || defined(LAB_DEVELOPMENT)
-
     CApplication::GetInstance().Run();
 
     return 0;
