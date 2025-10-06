70d6b97a964040391a290542f7ad24fe1be1d874
abazyshyn <maybebazishin@gmail.com>
2025-10-06 11:30:45 +0300

    feat: control menu

--- src/entry_point.cpp
+++ src/entry_point.cpp
@@ -8,6 +8,7 @@
 #include "pch.hpp"
 
 #include "window.hpp"
+#include "raw_data.hpp"
 #include "gui.hpp"
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
@@ -23,15 +24,46 @@ int main(int argc, char **argv)
 
     for (; window.IsRunning();) // Main loop
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        ImGui::ShowDemoWindow();
 
-        LAB_INFO_LOG("Render lab is running!");
+        ImGui::Begin("Control menu", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+        ImGui::SetWindowSize(ImVec2(300.0f, 500.0f));
+        ImGui::SetWindowPos("Control menu", ImVec2(0.0f, 0.0f));
+
+#pragma region Basic lighting scene
+
+        if (ImGui::CollapsingHeader("Basic lighting scene"))
+        {
+            static bool isRender = false;
+            if (ImGui::Button("Start scene"))
+            {
+                isRender = !isRender;
+            }
+
+            if (isRender)
+            {
+                LAB_INFO_LOG("Rendering basic lighting scene");
+                glClear(GL_COLOR_BUFFER_BIT);
+                glClearColor(0.2f, 1.0f, 0.2f, 1.0f);
+            }
+            else
+            {
+                glClear(GL_COLOR_BUFFER_BIT);
+                glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+            }
+        }
+        else
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        }
+
+#pragma endregion Basic lighting scene
+
+        ImGui::End();
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
---
+++ src/raw_data.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Lab
+{
+
+    /**
+     * @brief Returns cube's vertices
+     * @return Cube's vertices
+     */
+    std::vector<float> GetCubeVertices();
+
+} // namespace Lab
